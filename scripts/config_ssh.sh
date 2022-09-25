4b61e980e6b56b902dae5bd3572a2c29aa1ff58f
Santiago Verdu <santi@getlovage.com>
2022-09-25 15:26:20 -0600

    copy keys don't back up weird stuff

--- scripts/config_ssh.sh
+++ scripts/config_ssh.sh
@@ -4,11 +4,11 @@ set -e
 
 ip=$(terraform output -json | jq .ec2_public_ip.value | tr -d '"')
 
-sed -riE "
+sed -Eri "
 /# auto-ec2$/ {
   N
   s/(HostName).*$/\1 ${ip}/
 }
 " ~/.ssh/config
 
-ls -1 ~/.ssh/*.pub | xargs echo#ssh-copy-id -i 
+ls -1 ~/.ssh/*.pub | xargs -I{} ssh-copy-id -fi {} guru

7489d58fab113cee1b842c8744e48e0bda0fa69c
Santiago Verdu <erra@qmake.org>
2023-11-15 21:48:59 +0000

    aio

--- p/eb/app/app.py
+++ p/eb/app/app.py
@@ -1,32 +1,28 @@
 from fastapi import FastAPI
 import boto3
 from botocore.exceptions import NoCredentialsError
+from aiobotocore.session import get_session
 
-def s3_put_get_presign(bucket_name, object_key, object_data):
-    # Create an S3 client
-    s3_client = boto3.client('s3')
-
+async def s3_put_get_presign(bucket_name, object_key, object_data):
     try:
-        # Put an object
-        s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=object_data)
-
-        # Get the object
-        response = s3_client.get_object(Bucket=bucket_name, Key=object_key)
-        print("Object content:", response['Body'].read())
-
-        # Generate a pre-signed URL for the object
-        presigned_url = s3_client.generate_presigned_url('get_object',
-                                                         Params={'Bucket': bucket_name, 'Key': object_key},
-                                                         ExpiresIn=3600)
-        print("Pre-signed URL:", presigned_url)
-        return presigned_url
+        session = get_session()
+        async with session.create_client('s3') as s3_client:
+            await s3_client.put_object(Bucket=bucket_name, Key=object_key, Body=object_data)
+            response = await s3_client.get_object(Bucket=bucket_name, Key=object_key)
+            print(response)
+            presigned_url = await s3_client.generate_presigned_url(
+                'get_object',
+                Params={'Bucket': bucket_name, 'Key': object_key},
+                ExpiresIn=3600,
+            )
+            return presigned_url
     except NoCredentialsError:
         print("Credentials not available")
     except Exception as e:
         print(f"An error occurred: {e}")
 
 # Example usage
-bucket = 'elasticbeanstalk-us-east-1-837443695054'
+bucket = 'im-kan-kan'
 key = 'hardyhar'
 data = 'Hello, World!'
 
@@ -37,5 +33,5 @@ def root():
     return "wow, ok."
 
 @app.get("/s3")
-def s3():
-    return s3_put_get_presign(bucket, key, data)
+async def s3():
+    return await s3_put_get_presign(bucket, key, data)
